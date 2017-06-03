689790ca1b6a37ff7ea9a0ec1ee9a12e9272de08
Madhav Patel <madhav5589@gmail.com>
2017-06-03 16:15:19 -0700

    Initial commit
    Initial commit for the product management

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'products',
+    templateUrl: 'product-list.component.html'
+})
+
+export class ProductListComponent {
+    pageTitle: string = 'Product List';
+}
