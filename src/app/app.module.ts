0c811801aa1982391b02bf5eb106a457498733e8
Madhav Patel <madhav5589@gmail.com>
2017-06-09 23:32:17 -0700

    added product search filter and lifecycle hooks
    Interfaces, life cycle hooks, custom pipes and product filtering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent} from './products/product-list.component'
+import {ProductFilterPipe} from './products/product-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {IProduct} from './product';
 
 @Component({
     selector: 'products',
-    templateUrl: 'product-list.component.html'
+    templateUrl: 'product-list.component.html',
+    styleUrls:['product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
-    products: any[] = [
+    showImage: boolean = false;
+    listFilter: string = 'cart';
+    products: IProduct[] = [
     {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -16,7 +20,7 @@ export class ProductListComponent {
         "description": "Leaf rake with 48-inch wooden handle.",
         "price": 19.95,
         "starRating": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+        "imageUrl": ""
     },
     {
         "productId": 2,
@@ -26,6 +30,14 @@ export class ProductListComponent {
         "description": "15 gallon capacity rolling garden cart",
         "price": 32.99,
         "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+        "imageUrl": ""
     }];
+
+    ngOnInit() {
+        console.log("In OnInit");
+    }
+    
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
